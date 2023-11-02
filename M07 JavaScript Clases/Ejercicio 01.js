690c01da768d5b45557feefe92a3062dd200c456
rakor <delpunta@agro.uba.ar>
2023-11-01 23:28:55 -0300

    listo el 7

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -1,6 +1,8 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
+const { tieneEmail } = require("../M06 JavaScript Objetos/homework");
+
 function crearUsuario() {
    // Crea una Clase de ES6 o una función constructora llamada "Usuario".
    // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
@@ -10,18 +12,38 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario {
+      constructor (usuario, nombre, email, password) {
+         this.usuario = usuario
+         this.nombre = nombre
+         this.email = email
+         this.password = password
+      }
+      saludar () {
+         return ("Hola, mi nombre es " + this.nombre)
+      } 
+   } return Usuario
+     
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
-}
+      Usuario.prototype.saludar = function () {
+         return "Hello World!"
+      }
+}   
+       
+      
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
-   // [PISTA]: necesitarás utilizar el objeto "this".
+   // [PISTA]: necesitarás utilizar el objeto "this"
+   String.prototype.reverse = function () {
+      return this.split("").reverse().join("")
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
